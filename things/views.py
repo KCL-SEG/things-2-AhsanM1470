8b929b47b658bbd11706f2f66e332ecd01e88f40
AhsanM1470 <ahsanmahfuz04@gmail.com>
2023-12-08 00:33:30 +0000

    Fix bugs

--- things/views.py
+++ things/views.py
@@ -3,4 +3,10 @@ from .forms import ThingForm
 
 def home(request):
     form = ThingForm()
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form = ThingForm()
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
