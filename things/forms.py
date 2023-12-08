40d08456f7d70f96ce4209c4815082add47b7921
AhsanM1470 <ahsanmahfuz04@gmail.com>
2023-12-08 00:38:43 +0000

    Fix bug

--- things/forms.py
+++ things/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ThingForm(forms.Form):
     name = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea)
-    quantity = forms.IntegerField(widget=forms.NumberInput,)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
